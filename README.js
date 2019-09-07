4c6002dbfdca9f9ab76e5d96f7c36effb3927b6f
SDNotKing <34605562+SDNotKing@users.noreply.github.com>
2019-09-07 08:20:16 -0700

    Update and rename README.md to README.js

--- README.js
+++ README.js
@@ -1,4 +1,4 @@
-//META{"name":"ShowHiddenChannels","website":"https://github.com/mwittrien/BetterDiscordAddons/tree/master/Plugins/ShowHiddenChannels","source":"https://raw.githubusercontent.com/mwittrien/BetterDiscordAddons/master/Plugins/ShowHiddenChannels/ShowHiddenChannels.plugin.js"}*//
+//META{"name":"ShowHiddenChannels","website":"https://github.com/mwittrien/BetterDiscordAddons/tree/master/Plugins/ShowHiddenChannels","source":"https://raw.githubusercontent.com/SDNotKing/Discord-theme/master/README.js"}*//
 
 class ShowHiddenChannels {
 	getName () {return "ShowHiddenChannels";}
